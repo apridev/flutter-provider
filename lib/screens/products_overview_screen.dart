a3f756d1b7c2d658ee706c86c5b6bbd4bd20dfdb
Apridev <apriantofajar11@gmail.com>
2022-05-23 15:10:04 +0900

    product overview screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:latihan_provider/providers/cart.dart';
+import 'package:latihan_provider/widgets/badge.dart';
 import 'package:latihan_provider/widgets/product_grid.dart';
+import 'package:provider/provider.dart';
 
 import '../models/product.dart';
 import '../widgets/product_grid.dart';
@@ -13,6 +16,22 @@ class ProductsOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: Text('MyShop'),
+        actions: [
+          Consumer<Cart>(
+            builder: (context, value, ch) {
+              return Badge(
+              child: ch,
+              value: value.jumlah.toString()
+            );
+            },
+            child: IconButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/cart');
+                }, 
+                icon: Icon(Icons.shopping_cart)
+              ),
+          )
+        ],
       ),
       body: ProductGrid(),
     );
